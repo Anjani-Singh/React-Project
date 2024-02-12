64a1e289c9ffbdbb499c36fac45d746e705ca361
Anjani Singh <bhalla@Anjanis-MacBook-Air.local>
2024-02-12 19:31:55 +0530

    Basic Setup

---
+++ basic-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Youtube from './Youtube';
+
+function App() {
+  return (
+    <>
+      <h1>Vanilla Javascript App</h1>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
